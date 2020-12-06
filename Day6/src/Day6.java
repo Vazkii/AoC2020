21bdfe129706390b6b9001f41fcade47be99e1a8
Vasco Lavos <vazkii@hotmail.com>
2020-12-06 06:39:29 +0000

    day 6

---
+++ Day6/src/Day6.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day6 {
+	
+	public static void main(String[] args) throws IOException {
+		List<String> lines = new ArrayList<>();
+		Files.lines(Paths.get("input.txt"))
+			.forEach(lines::add);
+		
+		String curr = "";
+		int sum = 0;
+		boolean firstOfGroup = true;
+		
+		for(String s : lines) {
+			if(s.isEmpty()) {
+				sum += curr.length();
+				curr = "";
+				firstOfGroup = true;
+				continue;
+			}
+			
+			if(firstOfGroup) {
+				curr = s;
+				firstOfGroup = false;
+			} else {
+				String newStr = curr;
+				for(int i = 0; i < curr.length(); i++) {
+					char c = curr.charAt(i);
+					if(s.indexOf(c) < 0)
+						newStr = newStr.replaceAll(""+c, "");
+				}
+				curr = newStr;
+			}
+		}
+		
+		sum += curr.length();
+		
+		System.out.println("total " + sum);
+	}
+
+}
